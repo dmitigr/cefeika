4b9a8094725f9b0ddf7da6de6158f761f6f731ef
Dmitry Igrishin <dmitigr@gmail.com>
2019-11-12 13:45:32 +0300

    cmake: automatically consider dependencies of each component

--- cmake/dmitigr_cefeika.cmake
+++ cmake/dmitigr_cefeika.cmake
@@ -2,7 +2,14 @@
 # Copyright (C) Dmitry Igrishin
 # For conditions of distribution and use, see file LICENSE.txt
 
-# Note: dependant libraries must follows dependencies!
 set(dmitigr_cefeika_libraries
   util dt
   fcgi http img mulf pgfe ttpl url)
+
+set(dmitigr_cefeika_fcgi_deps util)
+set(dmitigr_cefeika_http_deps util dt)
+set(dmitigr_cefeika_img_deps  util)
+set(dmitigr_cefeika_mulf_deps util)
+set(dmitigr_cefeika_pgfe_deps util)
+set(dmitigr_cefeika_ttpl_deps util)
+set(dmitigr_cefeika_url_deps  util)
