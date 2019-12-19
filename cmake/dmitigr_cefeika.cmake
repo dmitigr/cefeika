1d16f339c71ef63d6698bb10fc93e5ad08791085
Dmitry Igrishin <dmitigr@gmail.com>
2019-12-19 18:26:57 +0300

    New library liuv - wrapper around libuv

--- cmake/dmitigr_cefeika.cmake
+++ cmake/dmitigr_cefeika.cmake
@@ -3,7 +3,7 @@
 # For conditions of distribution and use, see file LICENSE.txt
 
 set(dmitigr_cefeika_libraries
-  util dt rajson
+  util dt rajson liuv
   fcgi http img jrpc mulf pgfe ttpl url)
 
 set(dmitigr_cefeika_dt_deps util)
@@ -11,6 +11,7 @@ set(dmitigr_cefeika_fcgi_deps util)
 set(dmitigr_cefeika_http_deps dt util)
 set(dmitigr_cefeika_img_deps  util)
 set(dmitigr_cefeika_jrpc_deps rajson util)
+set(dmitigr_cefeika_liuv_deps util)
 set(dmitigr_cefeika_mulf_deps util)
 set(dmitigr_cefeika_pgfe_deps util)
 set(dmitigr_cefeika_rajson_deps util)
---
+++ lib/dmitigr/liuv/types_fwd.hpp
@@ -0,0 +1,21 @@
+// -*- C++ -*-
+// Copyright (C) Dmitry Igrishin
+// For conditions of distribution and use, see files LICENSE.txt or liuv.hpp
+
+#ifndef DMITIGR_LIUV_TYPES_FWD_HPP
+#define DMITIGR_LIUV_TYPES_FWD_HPP
+
+/**
+ * @brief The API.
+ */
+namespace dmitigr::liuv {
+
+/**
+ * @brief The implementation details.
+ */
+namespace detail {
+} // namespace detail
+
+} // namespace dmitigr::liuv
+
+#endif  // DMITIGR_LIUV_TYPES_FWD_HPP
