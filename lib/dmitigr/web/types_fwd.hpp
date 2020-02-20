ee7ebc3d11f234adc768073b6378f8177d4dd1ca
Dmitry Igrishin <dmitigr@gmail.com>
2020-02-20 18:02:22 +0300

    [new] library web

---
+++ lib/dmitigr/web/types_fwd.hpp
@@ -0,0 +1,23 @@
+// -*- C++ -*-
+// Copyright (C) Dmitry Igrishin
+// For conditions of distribution and use, see files LICENSE.txt or web.hpp
+
+#ifndef DMITIGR_WEB_TYPES_FWD_HPP
+#define DMITIGR_WEB_TYPES_FWD_HPP
+
+namespace dmitigr {
+
+/**
+ * @brief The API.
+ */
+namespace web {
+
+/**
+ * @brief The implementation details.
+ */
+namespace detail {
+} // namespace detail
+} // namespace web
+} // namespace dmitigr
+
+#endif  // DMITIGR_WEB_TYPES_FWD_HPP
