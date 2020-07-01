4e7f7633dbcd4fc659016a5bf736ea532ae52dfb
Dmitry Igrishin <dmitigr@gmail.com>
2020-07-01 15:21:14 +0300

    net: refactoring

--- lib/dmitigr/net/conversions.hpp
+++ lib/dmitigr/net/conversions.hpp
@@ -9,53 +9,56 @@
 
 #include <cstdint>
 #include <stdexcept>
-#include <type_traits>
 
 namespace dmitigr::net {
 
 /**
- * @brief Converts the raw `data` to the value of type `T` taking into
- * account the host's endianness.
- *
- * If the host architecture is big endian, then the `data` is copyied to
- * the result as is. Otherwise, the bytes of `data` is copied to the result
- * in reverse order.
+ * @brief If the host architecture is big endian, then the `src` is copyied to
+ * `dest` as is. Otherwise, the bytes of `src` is copied to `dest` in reverse order.
  */
-template<typename T>
-T conv(const char* const data, const std::size_t data_size)
+inline void conv(void* const dest, const std::size_t dest_size, const void* const src, const std::size_t src_size)
 {
-  DMITIGR_REQUIRE(data, std::invalid_argument);
-  DMITIGR_REQUIRE(data_size <= sizeof(T), std::invalid_argument);
-  T result{};
-  const auto data_ubytes = reinterpret_cast<const unsigned char*>(data);
-  const auto result_ubytes = reinterpret_cast<unsigned char*>(&result);
-  using Counter = std::remove_const_t<decltype (data_size)>;
+  DMITIGR_REQUIRE(dest, std::invalid_argument);
+  DMITIGR_REQUIRE(src, std::invalid_argument);
+  DMITIGR_REQUIRE(src_size <= dest_size, std::invalid_argument);
+  const auto src_ubytes = static_cast<const unsigned char*>(src);
+  const auto dest_ubytes = static_cast<unsigned char*>(dest);
   switch (endianness()) {
   case Endianness::big:
-    for (Counter i = 0; i < data_size; ++i)
-      result_ubytes[sizeof(T) - data_size + i] = data_ubytes[i];
+    for (std::size_t i = 0; i < src_size; ++i)
+      dest_ubytes[dest_size - src_size + i] = src_ubytes[i];
     break;
   case Endianness::little:
-    for (Counter i = 0; i < data_size; ++i)
-      result_ubytes[sizeof(T) - 1 - i] = data_ubytes[i];
+    for (std::size_t i = 0; i < src_size; ++i)
+      dest_ubytes[dest_size - 1 - i] = src_ubytes[i];
     break;
   case Endianness::unknown:
-    throw std::logic_error("unknown endianness");
+    throw std::logic_error{"unknown endianness"};
   }
-  return result;
 }
 
 /**
- * @returns The result of conversion of `value` to the value of type `Dest`
- * taking into account the host's endianness.
+ * @brief Converts the `data` to the value of type `T` taking into account the
+ * host's endianness.
+ *
+ * @returns The copy of `data` represented as a value of type `T`.
+ *
+ * @see conv()
  */
+template<typename T>
+inline T conv(const void* const data, const std::size_t data_size)
+{
+  T result{};
+  conv(&result, sizeof(result), data, data_size);
+  return result;
+}
+
+/// @overload
 template<typename Dest, typename Src>
 inline Dest conv(const Src& value)
 {
-  static_assert(sizeof(Dest) >= sizeof(Src),
-    "the size of destination type must not be less than the size of source type");
-  const char* const data = reinterpret_cast<const char*>(&value);
-  return conv<Dest>(data, sizeof(Src));
+  static_assert(sizeof(Dest) >= sizeof(Src));
+  return conv<Dest>(&value, sizeof(Src));
 }
 
 /// @overload
