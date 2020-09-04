43c444f03d6081a99bd9c11f8366d44e11d65925
Dmitry Igrishin <dmitigr@gmail.com>
2020-09-04 20:07:10 +0300

    pgfe: refactoring

--- lib/dmitigr/pgfe/std_system_error.cpp
+++ lib/dmitigr/pgfe/std_system_error.cpp
@@ -7,11 +7,6 @@
 
 namespace dmitigr::pgfe {
 
-DMITIGR_PGFE_INLINE const char* Client_error_category::name() const noexcept
-{
-  return "dmitigr_pgfe_client_error";
-}
-
 DMITIGR_PGFE_INLINE std::string Client_error_category::message(const int ev) const
 {
   std::string result(name());
@@ -22,11 +17,6 @@ DMITIGR_PGFE_INLINE std::string Client_error_category::message(const int ev) con
     result += literal;
   }
   return result;
-}
-
-DMITIGR_PGFE_INLINE const char* Server_error_category::name() const noexcept
-{
-  return "dmitigr_pgfe_server_error";
 }
 
 DMITIGR_PGFE_INLINE std::string Server_error_category::message(const int ev) const
@@ -41,40 +31,6 @@ DMITIGR_PGFE_INLINE std::string Server_error_category::message(const int ev) con
     result += literal;
   }
   return result;
-}
-
-// =============================================================================
-
-DMITIGR_PGFE_INLINE const Client_error_category& client_error_category() noexcept
-{
-  static const Client_error_category result;
-  return result;
-}
-
-DMITIGR_PGFE_INLINE const Server_error_category& server_error_category() noexcept
-{
-  static const Server_error_category result;
-  return result;
-}
-
-DMITIGR_PGFE_INLINE std::error_code make_error_code(Client_errc errc) noexcept
-{
-  return std::error_code{static_cast<int>(errc), client_error_category()};
-}
-
-DMITIGR_PGFE_INLINE std::error_code make_error_code(Server_errc errc) noexcept
-{
-  return std::error_code{static_cast<int>(errc), server_error_category()};
-}
-
-DMITIGR_PGFE_INLINE std::error_condition make_error_condition(Client_errc errc) noexcept
-{
-  return std::error_condition{static_cast<int>(errc), client_error_category()};
-}
-
-DMITIGR_PGFE_INLINE std::error_condition make_error_condition(Server_errc errc) noexcept
-{
-  return std::error_condition{static_cast<int>(errc), server_error_category()};
 }
 
 } // namespace dmitigr::pgfe
