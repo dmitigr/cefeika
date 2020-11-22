ef4f24cf9c574791e359c10235da46064a1b2106
Dmitry Igrishin <dmitigr@gmail.com>
2020-11-22 13:33:56 +0300

    pgfe: Na -> a

--- lib/dmitigr/pgfe/prepared_statement.hpp
+++ lib/dmitigr/pgfe/prepared_statement.hpp
@@ -33,7 +33,7 @@ namespace dmitigr::pgfe {
  */
 class Named_argument final {
 public:
-  /// Constructs the named argument bound to NULL.
+  /// Constructs the named argument bound to `NULL`.
   Named_argument(std::string name, std::nullptr_t) noexcept
     : name_{std::move(name)}
   {
@@ -46,7 +46,7 @@ public:
    * @par Effects
    * `(is_data_owner() == false)`.
    *
-   * @remarks No deep copy of `data` is performed.
+   * @remarks No deep copy of `data` performed.
    */
   Named_argument(std::string name, const Data* const data) noexcept
     : name_{std::move(name)}
@@ -136,7 +136,7 @@ private:
  *
  * @brief The alias of Named_argument.
  */
-using Na = Named_argument;
+using a = Named_argument;
 
 /**
  * @ingroup main
