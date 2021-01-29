eba190f3485cce0587cd166c604ae480b6f3c581
Dmitry Igrishin <dmitigr@gmail.com>
2021-01-29 13:32:49 +0300

    pgfe: fix test

--- test/pgfe/pgfe-unit-row_info.cpp
+++ test/pgfe/pgfe-unit-row_info.cpp
@@ -4,31 +4,24 @@
 
 #include "pgfe-unit.hpp"
 
-#include <dmitigr/pgfe/completion.hpp>
-#include <dmitigr/pgfe/row.hpp>
-#include <dmitigr/pgfe/row_info.hpp>
+namespace pgfe = dmitigr::pgfe;
+namespace testo = dmitigr::testo;
 
 int main(int, char* argv[])
-{
-  namespace pgfe = dmitigr::pgfe;
-  using namespace dmitigr::testo;
-
-  try {
-    auto conn = pgfe::test::make_connection();
-    conn->connect();
-
-    conn->perform(R"(select 1::integer theNumberOne, 1::integer "theNumberOne")");
-    const auto r = conn->wait_row();
-    ASSERT(r);
-    ASSERT(r.info().name_of(0) == "thenumberone");
-    ASSERT(r.info().name_of(1) == "theNumberOne");
-    ASSERT(r.info().index_of("thenumberone") == 0);
-    ASSERT(r.info().index_of("theNumberOne") == 1);
-  } catch (const std::exception& e) {
-    report_failure(argv[0], e);
-    return 1;
-  } catch (...) {
-    report_failure(argv[0]);
-    return 1;
-  }
+try {
+  auto conn = pgfe::test::make_connection();
+  conn->connect();
+  conn->execute([](auto&& row)
+  {
+    ASSERT(row.info().name_of(0) == "thenumberone");
+    ASSERT(row.info().name_of(1) == "theNumberOne");
+    ASSERT(row.info().index_of("thenumberone") == 0);
+    ASSERT(row.info().index_of("theNumberOne") == 1);
+  }, R"(select 1::integer theNumberOne, 1::integer "theNumberOne")");
+} catch (const std::exception& e) {
+  testo::report_failure(argv[0], e);
+  return 1;
+} catch (...) {
+  testo::report_failure(argv[0]);
+  return 1;
 }
