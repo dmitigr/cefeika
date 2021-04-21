74ae75c63601586265de2089b83f12b76eddddd1
Dmitry Igrishin <dmitigr@gmail.com>
2021-04-21 18:15:32 +0300

    misc: supplement assert example

--- test/misc/misc-assert.cpp
+++ test/misc/misc-assert.cpp
@@ -4,8 +4,22 @@
 
 #include <dmitigr/misc/assert.hpp>
 
+#include <chrono>
+#include <thread>
+
+void handle_terminate() noexcept
+{
+  std::cout << "terminate handled, waiting 5 seconds";
+  for (int i{}; i < 10; ++i) {
+    std::this_thread::sleep_for(std::chrono::milliseconds{500});
+    std::cout << "." << std::flush;;
+  }
+  std::cout << std::endl;
+}
+
 int main()
 {
+  std::set_terminate(&handle_terminate);
   dmitigr_assert(true);
   if (true)
     dmitigr_assert_always(false);
