93af6322cb6aac0a1edcd3363176ef028ecb81cf
Dmitry Igrishin <dmitigr@gmail.com>
2021-05-06 18:18:48 +0300

    ws: remove some #pragma GCC diagnostics

--- lib/dmitigr/ws/uwebsockets.hpp
+++ lib/dmitigr/ws/uwebsockets.hpp
@@ -8,17 +8,8 @@
 #ifdef __GNUG__
 // Disable some warnings of uWebSockets.
 #pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunknown-warning-option"
 #pragma GCC diagnostic ignored "-Wunused-variable"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-#pragma GCC diagnostic ignored "-Wsign-conversion"
-#pragma GCC diagnostic ignored "-Wold-style-cast"
-#pragma GCC diagnostic ignored "-Wshadow"
-#pragma GCC diagnostic ignored "-Wdocumentation"
-#pragma GCC diagnostic ignored "-Wnewline-eof"
-#pragma GCC diagnostic ignored "-Wcast-qual"
-#pragma GCC diagnostic ignored "-Wshorten-64-to-32"
 #endif
 
 #include "../thirdparty/uwebsockets/App.h"
