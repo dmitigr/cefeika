4885e0fbc086e392e1cdd7497fec006859e22351
Dmitry Igrishin <dmitigr@gmail.com>
2021-05-24 17:45:34 +0300

    progpar: refactoring

--- os/test/os-unit-proc_run.cpp
+++ os/test/os-unit-proc_run.cpp
@@ -17,13 +17,14 @@ void start()
 
 int main(int argc, char* argv[]) try {
   using namespace dmitigr::testo;
-  proc::prog_params = {argc, argv};
-  const std::string info = "[--detach]";
-  const auto [detach_o, all] = proc::prog_params.options("detach");
-  if (!all || !proc::prog_params.arguments().empty())
+  using proc::prog_params;
+  prog_params = {argc, argv};
+  const std::string info{"[--detach]"};
+  const auto [detach_o] = prog_params.options("detach");
+  if (prog_params.options().size() > 1 || !prog_params.arguments().empty())
     proc::usage(info);
 
-  const bool detach = detach_o.is_valid_throw_if_value();
+  const bool detach = detach_o.throw_if_value();
   proc::start(detach, start);
  } catch (const std::exception& e) {
   std::clog << argv[0] << ": " << e.what() << std::endl;
